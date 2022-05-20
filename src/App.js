acad24cd3dd544dbb62d7568c6171a21c17af1f7
aniket_narwade <aniket.narwade@zeksta.com>
2022-05-20 12:11:16 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+import HobbySelectorCheck from "./components/HobbySelectorCheck";
+import CustomerList from "./components/CustomerList";
+
+let App = () => {
+
+    return(
+        <React.Fragment>
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+                <a href="/" className="navbar-brand">React Redux - Structural Directives</a>
+            </nav>
+
+            <Provider store={store}>
+               <HobbySelectorCheck/>
+               <CustomerList/>
+            </Provider>
+
+         <div style={{marginBottom : '200px'}}/>
+        </React.Fragment>
+    );
+};
+export default App;
